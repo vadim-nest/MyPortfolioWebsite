7634fac580fc4052d9d35a408c0d3ac32cf94fe5
Vadim Gnezdyshkin <vadim@gne.me.uk>
2021-05-18 10:27:06 +0100

    Creating of My Portfolio Website

---
+++ script.js
@@ -0,0 +1,13 @@
+// Colours:
+//     white:           #FFFFFF
+//     black:           #222222
+//     yellow:          #FFCC02
+//     red:             #EE0000
+//     grey for laps:   #A3A3A3
+// 
+
+//     colors for buttons:
+//     yellow button :          #FFDB4D
+//     yellow button on hover : #FFD633
+//     yellow button on press : #FFCC00
+
